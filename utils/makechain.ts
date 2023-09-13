f52bf1972ee018cacce8c56fb6735f01ca981165
originn <orisomekh@gmail.com>
2023-09-13 13:02:07 +0300

    changes

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -11,18 +11,16 @@ Chat History:
 Follow Up Input: {question}
 Standalone question:`;
 
-const QA_PROMPT = `You are a helpful AI assistant. Use the following pieces of context to answer the question at the end.
-If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
-If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
-
-{context}
+const QA_PROMPT = `${process.env.QA_PROMPT || ""}{context}
 
 Question: {question}
 Helpful answer in markdown:`;
 
+const TEMPRATURE = parseFloat(process.env.TEMPRATURE || "0");
+
 export const makeChain = (vectorstore: PineconeStore) => {
   const model = new OpenAI({
-    temperature: 0, // increase temepreature to get more creative answers
+    temperature: TEMPRATURE, // increase temepreature to get more creative answers
     modelName: MODEL_NAME, 
   });
 
