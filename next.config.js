92c8a96aec5701479170cccb8a1ce34580664955
originn <orisomekh@gmail.com>
2023-10-13 15:44:01 +0300

    changes

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['solidcam.herokuapp.com'],
+  },
   webpack(config) {
     config.experiments = { ...config.experiments, topLevelAwait: true };
     return config;
