c639ef749897c9aebb7ec2c3a1473124728c8f9a
originn <orisomekh@gmail.com>
2023-10-13 15:15:19 +0300

    changes

--- server.cjs
+++ server.cjs
@@ -1,34 +1,32 @@
-//server.cjs
+const express = require('express');
+const { parse } = require('url');
+const next = require('next');
+const { init } = require('./socketServer.cjs');  // Import the init method
 
-const { createServer } = require("http");
-const { parse } = require("url");
-const { init } = require("./socketServer.cjs");
-const next = require("next");
-
-const dev = process.env.NODE_ENV !== "production";
+const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  const server = createServer((req, res) => {
+  const server = express();
+
+  // This ensures that all Next.js handling is still in place
+  server.all('*', (req, res) => {
     const parsedUrl = parse(req.url, true);
     handle(req, res, parsedUrl);
   });
 
-  // Initialize socket.io
-  const io = init(server);
-
-  io.on("connection", (socket) => {
-    console.log("Client connected!");
-
-    socket.on("disconnect", () => {
-      console.log("Client disconnected");
-    });
+  const httpServer = server.listen(process.env.PORT || 3000, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${process.env.PORT || 3000}`);
   });
 
-  const PORT = process.env.PORT || 3000;
-  server.listen(PORT, (err) => {
-    if (err) throw err;
-    console.log(`> Ready on http://localhost:${PORT}`);
+  // Initialize socket.io using the init method from socketServer.cjs
+  const io = init(httpServer);
+  io.on('connection', (socket) => {
+    console.log('Client connected!');
+    socket.on('disconnect', () => {
+      console.log('Client disconnected');
+    });
   });
 });
