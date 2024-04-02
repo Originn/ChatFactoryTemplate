92964d2d0b440868c25de291e204da8dc4f0323c
Ori Somekh <ori.somekh@solid.local>
2024-04-02 15:17:06 +0300

    mayjor login UI updates

--- pages/verify-email.tsx
+++ pages/verify-email.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useRouter } from 'next/router';
-import { applyActionCode } from 'firebase/auth';
+import { applyActionCode, reload } from 'firebase/auth';
 import { auth } from 'utils/firebase';
 
 const VerifyEmailPage = () => {
@@ -15,9 +15,10 @@ const VerifyEmailPage = () => {
       if (mode === 'verifyEmail' && oobCode) {
         try {
           await applyActionCode(auth, oobCode);
-          sessionStorage.setItem('emailVerified', 'true');
-          // Optionally, update other parts of your application state as needed
-          router.push('/');
+          if (auth.currentUser) {
+            await reload(auth.currentUser);
+            router.push('/'); // Now it's safe to navigate after reload
+          }
         } catch (error) {
           console.error('Error verifying email:', error);
           // Handle error (e.g., display an error message)
@@ -26,7 +27,7 @@ const VerifyEmailPage = () => {
     };
 
     handleEmailVerification();
-  }, [router]);
+  }, []);
 
   return (
     <div className="custom-verify-container">
