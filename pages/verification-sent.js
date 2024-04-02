12875aeb6b3d6c964a7f44f59a1420d3ac883fc7
Ori Somekh <ori.somekh@solid.local>
2024-04-02 15:50:19 +0300

    mayjor login UI updates

--- pages/verification-sent.js
+++ pages/verification-sent.js
@@ -1,43 +1,23 @@
-// pages/verification-sent.js
 import Image from 'next/image';
+import styles from './VerificationSent.module.css'; // Adjust the import path as needed
 
 export default function VerificationSent() {
-    // Inline styles
-    const containerStyle = {
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      justifyContent: 'center',
-      height: '100vh', // Use full viewport height
-      textAlign: 'center'
-    };
-  
-    const headerStyle = {
-      fontSize: '2em', // Larger font size
-      fontWeight: 'bold', // Bold font weight
-    };
-  
-    const paragraphStyle = {
-      fontSize: '1.2em', // Slightly larger font size
-    };
-  
-    const imageStyle = {
-      maxWidth: '100px', // Set a max width for your image
-      marginBottom: '20px', // Margin below the image
-    };
-  
-    return (
-      <div style={containerStyle}>
+  return (
+    <div className={styles.verificationSentContainer}>
+      <div className={styles.imageContainer}>
+        {/* Next.js Image component for optimized image loading */}
         <Image 
-          src="/solidcam.png" // Replace with the path to your image
+          src="/solidcam.png" // Replace with your actual image path
           alt="solidcam"
-          style={imageStyle}
+          width={100} // Adjust to the size of your actual image
+          height={100} // Adjust to the size of your actual image
+          className={styles.image}
         />
-        <h1 style={headerStyle}>Check Your Email</h1>
-        <p style={paragraphStyle}>
-          We've sent an email to the address you provided. Please click on the verification link in the email to complete your registration.
-        </p>
       </div>
-    );
-  }
-  
+      <h1 className={styles.header}>Check Your Email</h1>
+      <p className={styles.paragraph}>
+        We&apos;ve sent an email to the address you provided. Please click on the verification link in the email to complete your registration.
+      </p>
+    </div>
+  );
+}
