a2216529d9fc07e6488619a5f9ab1a22feea0500
Ori Somekh <ori.somekh@solid.local>
2024-04-02 15:17:06 +0300

    mayjor login UI updates

--- auth/AuthWrapper.tsx
+++ auth/AuthWrapper.tsx
@@ -1,5 +1,5 @@
 import React, { ReactElement, useEffect, useState, ReactNode } from 'react';
-import { onAuthStateChanged } from 'firebase/auth';
+import { onAuthStateChanged, reload } from 'firebase/auth';
 import { auth } from 'utils/firebase';
 import CustomLoginForm from './CustomLoginForm'; 
 import { useRouter } from 'next/router';
@@ -15,29 +15,40 @@ interface UserState {
   isAuthChecked: boolean; // Tracks if the initial check is done
 }
 
-const AuthWrapper = ({
-  children
-}: AuthWrapperProps): ReactElement | null => {
+const AuthWrapper = ({ children }: AuthWrapperProps): ReactElement | null => {
   const [userState, setUserState] = useState<UserState>({
     isLoading: true,
     isSignedIn: false,
     isEmailVerified: false,
     isAuthChecked: false
   });
-
   const router = useRouter(); 
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      setUserState({
-        isAuthChecked: true,
-        isLoading: false,
-        isSignedIn: !!user,
-        isEmailVerified: user ? user.emailVerified : false,
-      });
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        const shouldReload = sessionStorage.getItem('emailVerified') === 'true';
+        if (shouldReload) {
+          await reload(user);
+          sessionStorage.removeItem('emailVerified'); // Remove the flag after reloading
+        }
+        setUserState({
+          isAuthChecked: true,
+          isLoading: false,
+          isSignedIn: true,
+          isEmailVerified: user.emailVerified,
+        });
+      } else {
+        setUserState({
+          isAuthChecked: true,
+          isLoading: false,
+          isSignedIn: false,
+          isEmailVerified: false,
+        });
+      }
     });
-    
-    return () => unsubscribe(); // This function is called when the component unmounts
+  
+    return () => unsubscribe(); // Cleanup the subscription
   }, []);
 
   if (userState.isLoading) return <div>Loading...</div>;
