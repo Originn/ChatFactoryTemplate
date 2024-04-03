f7e43bfc4f7a43a7245212396574b2ba0254e633
Ori Somekh <ori.somekh@solid.local>
2024-04-03 10:29:23 +0300

    changing auth path in firebase

--- pages/acctmgmt/[...action].tsx
+++ pages/acctmgmt/[...action].tsx
@@ -4,10 +4,11 @@ import { applyActionCode, reload, verifyPasswordResetCode, confirmPasswordReset 
 import { auth } from 'utils/firebase';
 
 const ActionHandlerPage = () => {
-  const router = useRouter();
-  const [newPassword, setNewPassword] = useState('');
-  const [isResettingPassword, setIsResettingPassword] = useState(false);
-  const [actionCode, setActionCode] = useState('');
+    const router = useRouter();
+    const [newPassword, setNewPassword] = useState('');
+    const [isResettingPassword, setIsResettingPassword] = useState(false);
+    const [confirmationMessage, setConfirmationMessage] = useState('');
+    const [actionCode, setActionCode] = useState('');
 
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search);
@@ -52,17 +53,19 @@ const ActionHandlerPage = () => {
   }, [router]);
 
   const resetPassword = async () => {
-    if (!actionCode) {
-      console.error('Error: Action code is missing');
-      return;
-    }
     try {
+      // Assuming actionCode and newPassword are available and valid
       await confirmPasswordReset(auth, actionCode, newPassword);
-      setIsResettingPassword(false); // Reset complete, disable password reset mode
-      router.push('/login'); // Navigate to login after resetting
+      // Show confirmation message
+      setConfirmationMessage('Your password has been reset successfully.');
+
+      // Redirect after 2 seconds
+      setTimeout(() => {
+        router.push('/login'); // replace '/login' with your desired route
+      }, 2000);
     } catch (error) {
       console.error('Error resetting password:', error);
-      // Handle error
+      setConfirmationMessage('Failed to reset password. Please try again.');
     }
   };
 
@@ -70,36 +73,34 @@ const ActionHandlerPage = () => {
     <div className="passw-reset-popup-backdrop">
       <div className="passw-reset-popup">
         {isResettingPassword ? (
-          <>
-            <div className="passw-reset-popup-header">
-              <h2>Reset Your Password</h2>
-              <button
-                className="passw-reset-popup-close"
-                onClick={() => setIsResettingPassword(false)}
-              >
-                &times;
-              </button>
-            </div>
-            <div className="passw-reset-popup-body">
-              <input
-                type="password"
-                className="passw-reset-popup-body-input"
-                value={newPassword}
-                onChange={(e) => setNewPassword(e.target.value)}
-                placeholder="Enter new password"
-              />
-              <button
-                className="forgot-password-next-button"
-                onClick={resetPassword}
-              >
-                Reset Password
-              </button>
-            </div>
-          </>
+          <div className="passw-reset-popup-body">
+            {confirmationMessage ? (
+              <div className={confirmationMessage.startsWith("Your password has") ? "confirmation-message" : "error-message"}>
+                {confirmationMessage}
+              </div>
+            ) : (
+              <>
+                <h2>Reset Your Password</h2>
+                <input
+                  type="password"
+                  className="passw-reset-popup-body-input"
+                  value={newPassword}
+                  onChange={(e) => setNewPassword(e.target.value)}
+                  placeholder="Enter new password"
+                />
+                <button
+                  className="passw-reset-next-button"
+                  onClick={resetPassword}
+                >
+                  Reset Password
+                </button>
+              </>
+            )}
+          </div>
         ) : (
           <div className="passw-reset-popup-body">
             <h1>Processing your request...</h1>
-            <p>Please wait...</p> {/* Display a loading message or spinner */}
+            <p>Please wait...</p>
           </div>
         )}
       </div>
