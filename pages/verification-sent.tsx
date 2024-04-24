aadcd536389654e50da8f4b01d8de6199b5ffcbd
Ori Somekh <ori.somekh@solid.local>
2024-04-24 10:50:57 +0300

    added text to verify email

--- pages/verification-sent.tsx
+++ pages/verification-sent.tsx
@@ -1,5 +1,3 @@
-
-
 import styles from '@/styles/VerificationSent.module.css';
 
 export default function VerificationSent() {
@@ -18,6 +16,9 @@ export default function VerificationSent() {
       <h1 className={styles.header}>Check Your Email</h1>
       <p className={styles.paragraph}>
         We&apos;ve sent an email to the address you provided. Please click on the verification link in the email to complete your registration.
+      </p>
+      <p className={styles.paragraph}>
+        If you don't see our email shortly, please check your junk or spam folder.
       </p>
     </div>
   );
