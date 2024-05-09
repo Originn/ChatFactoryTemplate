dc5f91543c2addeadc58c8cee7447b2fa86176ef
Ori Somekh <ori.somekh@solid.local>
2024-05-09 10:18:59 +0300

    privacy-policy fix

--- components/layout.tsx
+++ components/layout.tsx
@@ -29,8 +29,8 @@ if (process.env.NODE_ENV === PRODUCTION_ENV) {
 
 export default function Layout({ children, theme, toggleTheme }: LayoutProps) {
   return (
-    <div className={`mx-auto flex flex-col space-y-4 ${theme === 'dark' ? 'dark' : ''}`}>
-      <header className={`w-full sticky top-0 z-40 ${theme === 'light' ? 'bg-white' : 'bg-dark-header'}`}>
+    <div className={`flex flex-col min-h-screen mx-auto ${theme}`}>
+      <header className={`sticky top-0 z-40 w-full ${theme === 'light' ? 'bg-white' : 'bg-dark-header'}`}>
         <div className="h-16 border-b border-b-slate-200 py-4">
           <nav className="ml-4 pl-6 flex items-center justify-start">
             <button onClick={toggleTheme} className="bg-gray-200 dark:bg-gray-600 p-2 rounded-full ml-4">
@@ -50,7 +50,7 @@ export default function Layout({ children, theme, toggleTheme }: LayoutProps) {
         </div>
       </header>
       <div>
-        <main className="flex w-full flex-1 flex-col overflow-hidden">
+        <main className="flex flex-col flex-1 overflow-hidden">
           {children}
         </main>
       </div>
