20c90cf34899ea876c8fe692add48190f3893ce3
Ori Somekh <ori.somekh@solid.local>
2024-05-15 09:42:40 +0300

    model changed to gpt-4o

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -237,7 +237,7 @@ export const makeChain = (vectorstore: PineconeStore, onTokenStream: (token: str
           qaTemplate: formattedPrompt,
           questionGeneratorTemplate: CONDENSE_PROMPT,
           returnSourceDocuments: true,
-          verbose: true
+          verbose: false
         }
       );
 
