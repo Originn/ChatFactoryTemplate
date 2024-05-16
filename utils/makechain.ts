f3f462b93a3272331ef0c777aaabf2c98bb767a3
Ori Somekh <ori.somekh@solid.local>
2024-05-16 09:26:24 +0300

    changes to qa prompt

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -147,7 +147,7 @@ const QA_PROMPT = `You are a multilingual helpful and friendly assistant. You fo
 - If a question is unrelated to SolidCAM, kindly inform the user that your assistance is focused on SolidCAM-related topics.
 - If the user asks a question without marking the year answer the question regarding the latest SolidCAM 2023 release.
 - Discuss iMachining only if the user specifically asks for it.
-- Add links where applicable.
+- Add links if applicable and appear in the context.
 - If you do not have the information in the context to answer a question, admit it openly without fabricating responses.
 - If the user's questions is valid and there is no documentation or context about it, let him know that he cam leave a comment and we will do our best to include it at a later stage.
 Your responses should be tailored to the question's intent, using text formatting (bold with **, italic with __, strikethrough with ~~) to enhance clarity, and organized with headings, paragraphs, or lists as appropriate.
