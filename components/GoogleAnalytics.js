600d7357adb03a5ccd4e41afea0515a3fa123433
Ori Somekh <ori.somekh@solid.local>
2024-05-29 16:21:46 +0300

    added Google analytics

---
+++ components/GoogleAnalytics.js
@@ -0,0 +1,42 @@
+// components/GoogleAnalytics.js
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+const TRACKING_ID = process.env.NEXT_PUBLIC_TRACKING_ID; // Replace with your GA4 Measurement ID
+
+const GoogleAnalytics = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      window.gtag('config', TRACKING_ID, {
+        page_path: url,
+      });
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = `https://www.googletagmanager.com/gtag/js?id=${TRACKING_ID}`;
+    script.async = true;
+    document.head.appendChild(script);
+
+    const scriptTag = document.createElement('script');
+    scriptTag.innerHTML = `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', '${TRACKING_ID}');
+    `;
+    document.head.appendChild(scriptTag);
+  }, []);
+
+  return null;
+};
+
+export default GoogleAnalytics;
