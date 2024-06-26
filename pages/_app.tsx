af3206b4af928157762092b4183f8815309325fb
Ori Somekh <ori.somekh@solid.local>
2024-06-26 14:17:06 +0300

    replaced buckets

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,25 +1,29 @@
-// pages/_app.tsx
 import '@/styles/base.css';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
-import AuthWrapper from '../auth/AuthWrapper'; 
+import AuthWrapper from '../auth/AuthWrapper';
 import { useRouter } from 'next/router';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
-
 
   const noAuthRequired = ['/verify-email', '/verification-sent', '/password-reset-confirmation', '/acctmgmt','/privacy-policy'];
   const isAuthRequired = !noAuthRequired.some(path => router.pathname.startsWith(path));
 
   return (
     <>
-      {isAuthRequired ? ( 
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="Visit our website to further your knowledge about SolidCAM using our LLM ChatBot. Get personalized assistance and detailed information to enhance your experience." />
+        <title>SolidCAM Chat</title>
+      </Head>
+      {isAuthRequired ? (
         <AuthWrapper>
           <Component {...pageProps} />
         </AuthWrapper>
       ) : (
-        <Component {...pageProps} /> 
+        <Component {...pageProps} />
       )}
     </>
   );
