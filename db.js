79ede683d1c9f8753be23f9be006ddfda07c5249
Ori Somekh <ori.somekh@solid.local>
2024-09-08 10:49:30 +0300

    testing for staging

--- db.js
+++ db.js
@@ -5,6 +5,7 @@ if (process.env.NODE_ENV !== 'production') {
   dotenv.config();
 }
 
+console.log(process.env.NODE_ENV)
 const isProduction = process.env.NODE_ENV === 'production';
 const poolConfig = isProduction
   ? {
