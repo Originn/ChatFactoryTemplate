c322232a3916e9f394a78ef9a8ee19bad3528da6
Ori Somekh <ori.somekh@solid.local>
2024-10-17 23:10:26 +0300

    added tracking using auth id

--- utils/tracking.ts
+++ utils/tracking.ts
@@ -13,7 +13,7 @@ export const handleWebinarClick = (url: string) => {
     window.gtag('event', 'webinar_source_click', {
       event_category: 'Webinars',
       event_label: 'Webinar',
-      user_id: auth.currentUser?.email,
+      user_id: auth.currentUser?.uid,
       value: url,
     });
   }
@@ -25,7 +25,7 @@ export const handleDocumentClick = (url: string) => {
     window.gtag('event', 'document_source_click', {
       event_category: 'Documents',
       event_label: 'Document',
-      user_id: auth.currentUser?.email,
+      user_id: auth.currentUser?.uid,
       value: url,
     });
   }
@@ -37,7 +37,7 @@ export const measureFirstTokenTime = (timeDifference: number) => {
     window.gtag('event', 'first_token_response_time', {
       event_category: 'ChatBot',
       event_label: 'Time from Submit to First Token',
-      user_id: auth.currentUser?.email,
+      user_id: auth.currentUser?.uid,
       value: timeDifference,
     });
   }
@@ -49,7 +49,7 @@ export const handleMicClickEvent = () => {
     window.gtag('event', 'mic_click', {
       event_category: 'Interaction',
       event_label: 'Microphone Click',
-      user_id: auth.currentUser?.email,
+      user_id: auth.currentUser?.uid,
     });
   }
 };
@@ -59,8 +59,23 @@ export const handleSubmitClick = () => {
     window.gtag('event', 'submit_click', {
       event_category: 'ChatBot',
       event_label: 'User Submission',
-      user_id: auth.currentUser?.email,
+      user_id: auth.currentUser?.uid,
     });
   }
 };
 
+export const setUserIdForAnalytics = () => {
+  if (typeof window !== 'undefined' && window.gtag) {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        window.gtag('config', process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID, {
+          'user_id': user.uid
+        });
+      } else {
+        window.gtag('config', process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID, {
+          'user_id': undefined
+        });
+      }
+    });
+  }
+};
