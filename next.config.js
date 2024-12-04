39913c48b6f45ab5d02c54f0a53abd5096e80f37
Ori Somekh <ori.somekh@solid.local>
2024-12-04 14:06:37 +0200

    adjusting textareahight upon iframe

--- next.config.js
+++ next.config.js
@@ -17,6 +17,27 @@ const nextConfig = {
     GOOGLE_APPLICATION_CREDENTIALS: process.env.GOOGLE_APPLICATION_CREDENTIALS,
     NEXT_PUBLIC_GA_MEASUREMENT_ID: process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID,
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*', // Or restrict to specific origins like 'http://127.0.0.1:5500'
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET, POST, PUT, DELETE, OPTIONS',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'Content-Type, Authorization',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
